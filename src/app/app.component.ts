9bfc67b4d8aadbf422072f06b85684fca5994189
Jenil Shah <101411341@georgebrown.ca>
2025-04-02 15:46:34 -0400

    Labtest 2

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MissionlistComponent } from './missionlist/missionlist.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [MissionlistComponent, HttpClientModule, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '101411341-lab-test2-comp3133';
+  yearFilter: string = '';
+  launchStatus: string = '';
+  landingStatus: string = '';
+
+  applyFilters() {
+    // This will be handled by the missionlist component
+  }
+
+  resetFilters() {
+    this.yearFilter = '';
+    this.launchStatus = '';
+    this.landingStatus = '';
+    // This will be handled by the missionlist component
+  }
+}
